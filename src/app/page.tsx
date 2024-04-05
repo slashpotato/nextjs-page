3ee267140ad445caa2eb9cbe1acf79771f1ada91
slashpotato <slashpotato@proton.me>
2024-04-05 17:32:49 +0400

    social links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Page() {
     return (
-        <main className=" w-20">
-            <div className="block">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem facere quisquam ducimus sapiente labore, asperiores error rem! Hic excepturi vero error est accusamus animi, porro vel consectetur fugit officia aspernatur! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus quidem nam exercitationem! Facere ut dolorem repudiandae dignissimos consectetur. Natus aperiam minus libero id voluptas architecto blanditiis ab reprehenderit, nisi quibusdam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nam est laborum voluptate asperiores placeat aliquid aliquam laudantium dolorum vel aspernatur, quas blanditiis excepturi vitae voluptas ipsum, esse hic porro!
+        <main className="classic">
+            <div className="pl-10 pt-12 text-3xl">
+                <ol id="social-ol">
+                    <li><Link href={"https://ovk.to/slashpotato"}><Image src={"/ovk.svg"} alt={"openvk logo"} width={32} height={16} /> OpenVK</Link></li>
+                    <li><Link href={"https://t.me/slashpotato"}><Image src={"/telegram.svg"} alt={"telegram logo"} width={32} height={16} /> Telegram</Link></li>
+                    <li><Link href={"https://discord.gg/WFaGuxgNPp"}><Image src={"/discord.svg"} alt={"discord logo"} width={32} height={16} /> Discord</Link></li>
+                    <li><Link href={"https://github.com/slashpotato"}><Image src={"/github.svg"} alt={"github logo"} width={32} height={16} /> GitHub</Link></li>
+                </ol>
             </div>
         </main>
     );
